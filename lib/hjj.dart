81dfb052bf177d85603a3ed07c3be7eb24866390
Aakash Siddhpura <npdangar.weapplinse@gmail.com>
2023-11-22 17:23:16 +0530

    add

---
+++ lib/hjj.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class htkkp extends StatefulWidget {
+  const htkkp({super.key});
+
+  @override
+  State<htkkp> createState() => _htkkpState();
+}
+
+class _htkkpState extends State<htkkp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
